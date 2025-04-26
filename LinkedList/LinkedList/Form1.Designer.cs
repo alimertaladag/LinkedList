4ecbd4a710b6b78b63c4756f5411e11222cd4b49
alimertaladag <alimertaladag@gmail.com>
2025-04-26 16:12:28 +0300

    LinkedList
    LinkedList Application – Technical Overview
    The LinkedList application is a C# WinForms project that implements a custom singly linked list data structure with dynamic node management.
    
    Key technical features:
    
    Node Operations: Supports adding, deleting, and searching nodes within the list.
    
    Dynamic Visualization: Updates the linked list structure in the UI after each operation.
    
    Custom Data Structure: Implements a Node class (data + next pointer) and LinkedList management class.
    
    User Interface: Built using Windows Forms to allow user-driven list manipulation in real time.
    
    Error Handling: Provides feedback for invalid operations (e.g., searching non-existent nodes).
    
    Educational Focus: Designed for learning and practicing data structures with immediate visual feedback.
    
    The application is ideal for students and developers seeking hands-on experience with linked list algorithms and logic.

---
+++ LinkedList/LinkedList/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LinkedListApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.NumericUpDown numIndex;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.numIndex = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numIndex)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(150, 85);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(120, 30);
+            this.btnInsert.TabIndex = 2;
+            this.btnInsert.Text = "Ekle";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(150, 125);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(120, 30);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(150, 50);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(120, 20);
+            this.txtData.TabIndex = 4;
+            // 
+            // numIndex
+            // 
+            this.numIndex.Location = new System.Drawing.Point(150, 170);
+            this.numIndex.Name = "numIndex";
+            this.numIndex.Size = new System.Drawing.Size(120, 20);
+            this.numIndex.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 211);
+            this.Controls.Add(this.numIndex);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "LinkedList Uygulaması";
+            ((System.ComponentModel.ISupportInitialize)(this.numIndex)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace LinkedListApp
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class LinkedList
+    {
+        public Node Head;
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public void Append(Node newNode)
+        {
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void Insert(Node newNode, int index)
+        {
+            if (index == 0)
+            {
+                newNode.Next = Head;
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                for (int i = 0; i < index - 1 && current != null; i++)
+                {
+                    current = current.Next;
+                }
+                if (current != null)
+                {
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                }
+            }
+        }
+
+        public int Delete(int index)
+        {
+            if (Head == null) return -1;
+
+            if (index == 0)
+            {
+                int deletedValue = Head.Data;
+                Head = Head.Next;
+                return deletedValue;
+            }
+
+            Node current = Head;
+            Node previous = null;
+
+            for (int i = 0; i < index && current != null; i++)
+            {
+                previous = current;
+                current = current.Next;
+            }
+
+            if (current != null)
+            {
+                int deletedValue = current.Data;
+                previous.Next = current.Next;
+                return deletedValue;
+            }
+
+            return -1;
+        }
+
+        public List<int> GetAllData()
+        {
+            List<int> list = new List<int>();
+            Node current = Head;
+            while (current != null)
+            {
+                list.Add(current.Data);
+                current = current.Next;
+            }
+            return list;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        LinkedList linkedList = new LinkedList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtData.Text, out int data))
+            {
+                int index = (int)numIndex.Value;
+                Node newNode = new Node(data);
+                linkedList.Insert(newNode, index);
+                RefreshList();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen geçerli bir sayı girin.");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int index = (int)numIndex.Value;
+            linkedList.Delete(index);
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            listBox1.Items.Clear();
+            foreach (int data in linkedList.GetAllData())
+            {
+                listBox1.Items.Add(data);
+            }
+        }
+    }
+}
